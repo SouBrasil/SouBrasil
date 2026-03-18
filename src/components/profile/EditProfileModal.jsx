673a080d241ccf4363af82829cc4cc4659d24395
Sou Brasil <clubesoubrasil@gmail.com>
2026-03-18 21:50:08 +0000

    File changes

--- src/components/profile/EditProfileModal.jsx
+++ src/components/profile/EditProfileModal.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { maskCPF } from '@/utils/masks';
 import { base44 } from '@/api/base44Client';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
