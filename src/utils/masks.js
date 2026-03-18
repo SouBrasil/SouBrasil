cbed04d5300d5ad06ceb4e13102f29e38f3bd09d
Sou Brasil <clubesoubrasil@gmail.com>
2026-03-18 22:15:23 +0000

    File changes

--- src/utils/masks.js
+++ src/utils/masks.js
@@ -1,3 +1,15 @@
+export function maskPhone(value) {
+  const digits = value.replace(/\D/g, '').slice(0, 11);
+  if (digits.length <= 10) {
+    return digits
+      .replace(/(\d{2})(\d)/, '($1) $2')
+      .replace(/(\d{4})(\d{1,4})$/, '$1-$2');
+  }
+  return digits
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d{1,4})$/, '$1-$2');
+}
+
 export function maskCPF(value) {
   const digits = value.replace(/\D/g, '').slice(0, 11);
   return digits
