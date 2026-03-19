c9876f1135bfb3bb84c76b84aa6668708c3c83ab
Sou Brasil <clubesoubrasil@gmail.com>
2026-03-19 22:44:31 +0000

    File changes

--- src/components/adminpanel/AdminPanelPartners.jsx
+++ src/components/adminpanel/AdminPanelPartners.jsx
@@ -141,7 +141,8 @@ export default function AdminPanelPartners({ session }) {
 
       {subMenu === 'deleted' && <AdminPanelDeletedPartners session={session} />}
 
-      {subMenu === 'active' && <div className="space-y-4">
+      {subMenu === 'active' && (
+      <div className="space-y-4">
       <div className="flex flex-col sm:flex-row gap-3">
         <div className="relative flex-1">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-slate-400" />
