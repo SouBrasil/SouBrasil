a2c988fb846d57d107e3952a6d0c66800bc741e1
Sou Brasil <clubesoubrasil@gmail.com>
2026-03-22 23:28:26 +0000

    File changes

--- src/components/pricing/CheckoutModal.jsx
+++ src/components/pricing/CheckoutModal.jsx
@@ -221,10 +221,11 @@ export default function CheckoutModal({ plan, planType = 'client', onClose, user
   };
 
   const formatCpf = (v) => {
-    const n = v.replace(/\D/g, '').slice(0, 11);
-    return n.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
-            .replace(/(\d{3})(\d{3})(\d{0,3})/, '$1.$2.$3')
-            .replace(/(\d{3})(\d{0,3})/, '$1.$2');
+    const digits = v.replace(/\D/g, '').slice(0, 11);
+    return digits
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
   };
 
   return (
